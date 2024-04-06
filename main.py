38fe4d71598e2a0b5b8c43088aedafbbdb1aa356
Uskov_Aleksandr <Alex_us_01@mail.ru>
2024-04-06 14:54:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from pprint import pprint
+# читаем адресную книгу в формате CSV в список contacts_list
+import csv
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+  rows = csv.reader(f, delimiter=",")
+  contacts_list = list(rows)
+# pprint(contacts_list)
+# print(contacts_list)
+
+
+# # TODO 1: выполните пункты 1-3 ДЗ
+# # ваш код
+contacts_list_1 = contacts_list[1::]
+
+for element in contacts_list_1:
+  # print(element)
+
+  if len(element[0]) and len(element[1]) and len(element[2]) != 0:
+    # print(element)
+    set_1 = set()
+    for el_2 in element:
+      set_1.update(element)
+    print(set_1)
+
+
+
+#
+# # TODO 2: сохраните получившиеся данные в другой файл
+# # код для записи файла в формате CSV
+# with open("phonebook.csv", "w", encoding="utf-8") as f:
+#   datawriter = csv.writer(f, delimiter=',')
+#   # Вместо contacts_list подставьте свой список
+#   datawriter.writerows(contacts_list)
+
+
